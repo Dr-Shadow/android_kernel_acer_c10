59724952bd4d1b74f40e9a07a2c4664981e767f0
Dr-Shadow <dr-shadow@dr-shadow.eu>
2013-12-17 03:46:21 +0100

    Release 2.2

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
-make -j12 TARGET_PRODUCT=aa66
-mkdir -p out
+make -j4 TARGET_PRODUCT=aa66
+mkdir -p out/system/lib/modules
+for line in $(find . -name *.ko | grep -v system/lib); do 
+     echo "$line"
+     cp "$line" out/system/lib/modules/
+done
 ../mediatek/build/tools/mkimage arch/arm/boot/zImage KERNEL > out/zImage
 repack-MT65xx.pl -boot out/zImage ../ramdisk out/boot.img
